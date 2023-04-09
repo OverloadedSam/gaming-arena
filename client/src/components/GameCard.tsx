3268c5cd7fe96e7d3100053060953f62971fdfe9
Samdeesh <samdeeshsingh001@gmail.com>
2023-04-09 15:25:33 +0530

    Build platform icon list component to show platform icons

---
+++ client/src/components/GameCard.tsx
@@ -0,0 +1,23 @@
+import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { Game } from '../hooks/useGames';
+import PlatformIconList from './PlatformIconList';
+
+interface GameCardProps {
+  game: Game;
+}
+
+const GameCard = ({ game }: GameCardProps) => {
+  return (
+    <Card borderRadius={10} overflow={'hidden'}>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize='2xl'>{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((platform) => platform.platform)}
+        />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
