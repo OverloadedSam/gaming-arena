4df8b305f46f9b3f76d868a2dc319af3c7e83c9e
Samdeesh <samdeeshsingh001@gmail.com>
2023-04-09 12:46:19 +0530

    Build basic navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,31 @@
 import { Button } from '@chakra-ui/react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <ChakraProvider>
       <div className='app'>
-        <Button colorScheme='blue'>Button</Button>
+        <Grid
+          templateAreas={{
+            base: `'nav' 'main'`,
+            lg: `'nav nav' 'aside main'`,
+          }}
+        >
+          <GridItem area='nav'>
+            <NavBar />
+          </GridItem>
+
+          <Show above='lg'>
+            <GridItem area='aside' bg='coral'>
+              Aside
+            </GridItem>
+          </Show>
+
+          <GridItem area='main' bg='gold'>
+            Main
+          </GridItem>
+        </Grid>
       </div>
     </ChakraProvider>
   );
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { HStack, Image, Text } from '@chakra-ui/react';
+import logo from '../assets/logo.webp';
+
+const NavBar = () => {
+  return <div>
+    <HStack>
+      <Image src={logo} boxSize='60px' />
+      <Text>Gaming Arena</Text>
+    </HStack>
+  </div>;
+};
+
+export default NavBar;
