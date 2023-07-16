605b2035bd4b79b293c0cdd4203c47d64d140903
Samdeesh <samdeeshsingh001@gmail.com>
2023-07-16 20:39:41 +0530

    Extract entity interface to a separate file

--- client/src/components/GameCard.tsx
+++ client/src/components/GameCard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import notFound from '../assets/not-found.jpg';
-import { Game } from '../hooks/useGames';
+import { Game } from '../entities/Game';
 import getCroppedImageUrl from '../services/image-url';
 import CriticScore from './CriticScore';
 import Emoji from './Emoji';
